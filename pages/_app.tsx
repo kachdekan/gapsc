e0a660daadfb3d8ce887dc938e7cca76e476dcb7
Joel Adie <adiejoel14+monarch@gmail.com>
2023-12-17 22:58:10 +0100

    Add project ID

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { Alfajores, Celo } from "@celo/rainbowkit-celo/chains";
-// import celoGroups from "@celo/rainbowkit-celo/lists";
+import celoGroups from "@celo/rainbowkit-celo/lists";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
@@ -7,18 +7,20 @@ import { WagmiConfig, configureChains, createConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import Layout from "../components/Layout";
 import { Urbanist } from "next/font/google";
-import "../styles/globals.css";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 import { ThemeProvider } from "@material-tailwind/react";
+import "../styles/globals.css";
+
+const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID!;
 
 const { chains, publicClient } = configureChains(
   [Celo, Alfajores],
   [publicProvider()]
 );
 
-const connectors = [new InjectedConnector({ chains })];
+const connectors = celoGroups({ chains, projectId });
 
 const appInfo = {
   appName: "Games And Players Network",
