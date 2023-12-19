5e5a4c4d73fba7269e5dfe435b380f4263ada013
Joel Adie <adiejoel14+monarch@gmail.com>
2023-12-19 13:43:46 +0100

    Change BrowserProvider argument

--- utils/transactions.ts
+++ utils/transactions.ts
@@ -2,195 +2,195 @@ import { ethers, Contract, BrowserProvider, parseEther } from "ethers";
 
 const GAP_CONTRACT_ADDRESS = "0x6c6fB8a6eD54f73077c96a8E8B80F220D49fe860";
 
-const abi = [
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "_registrationFee",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "firstPrize",
-        type: "uint256",
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "secondPrize",
-        type: "uint256",
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "thirdPrize",
-        type: "uint256",
-      },
-    ],
-    name: "TournamentClosed",
-    type: "event",
-  },
-  {
-    inputs: [],
-    name: "endTournament",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "firstWinner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "organizer",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "register",
-    outputs: [],
-    stateMutability: "payable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    name: "registeredParticipants",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "registrationFee",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "secondWinner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_firstWinner",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "_secondWinner",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "_thirdWinner",
-        type: "address",
-      },
-    ],
-    name: "setWinners",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "thirdWinner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "totalPrizePool",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "tournamentStatus",
-    outputs: [
-      {
-        internalType: "enum GamingTournament.TournamentStatus",
-        name: "",
-        type: "uint8",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    stateMutability: "payable",
-    type: "receive",
-  },
-];
+// const abi = [
+//   {
+//     inputs: [
+//       {
+//         internalType: "uint256",
+//         name: "_registrationFee",
+//         type: "uint256",
+//       },
+//     ],
+//     stateMutability: "nonpayable",
+//     type: "constructor",
+//   },
+//   {
+//     anonymous: false,
+//     inputs: [
+//       {
+//         indexed: false,
+//         internalType: "uint256",
+//         name: "firstPrize",
+//         type: "uint256",
+//       },
+//       {
+//         indexed: false,
+//         internalType: "uint256",
+//         name: "secondPrize",
+//         type: "uint256",
+//       },
+//       {
+//         indexed: false,
+//         internalType: "uint256",
+//         name: "thirdPrize",
+//         type: "uint256",
+//       },
+//     ],
+//     name: "TournamentClosed",
+//     type: "event",
+//   },
+//   {
+//     inputs: [],
+//     name: "endTournament",
+//     outputs: [],
+//     stateMutability: "nonpayable",
+//     type: "function",
+//   },
+//   {
+//     inputs: [],
+//     name: "firstWinner",
+//     outputs: [
+//       {
+//         internalType: "address",
+//         name: "",
+//         type: "address",
+//       },
+//     ],
+//     stateMutability: "view",
+//     type: "function",
+//   },
+//   {
+//     inputs: [],
+//     name: "organizer",
+//     outputs: [
+//       {
+//         internalType: "address",
+//         name: "",
+//         type: "address",
+//       },
+//     ],
+//     stateMutability: "view",
+//     type: "function",
+//   },
+//   {
+//     inputs: [],
+//     name: "register",
+//     outputs: [],
+//     stateMutability: "payable",
+//     type: "function",
+//   },
+//   {
+//     inputs: [
+//       {
+//         internalType: "address",
+//         name: "",
+//         type: "address",
+//       },
+//     ],
+//     name: "registeredParticipants",
+//     outputs: [
+//       {
+//         internalType: "bool",
+//         name: "",
+//         type: "bool",
+//       },
+//     ],
+//     stateMutability: "view",
+//     type: "function",
+//   },
+//   {
+//     inputs: [],
+//     name: "registrationFee",
+//     outputs: [
+//       {
+//         internalType: "uint256",
+//         name: "",
+//         type: "uint256",
+//       },
+//     ],
+//     stateMutability: "view",
+//     type: "function",
+//   },
+//   {
+//     inputs: [],
+//     name: "secondWinner",
+//     outputs: [
+//       {
+//         internalType: "address",
+//         name: "",
+//         type: "address",
+//       },
+//     ],
+//     stateMutability: "view",
+//     type: "function",
+//   },
+//   {
+//     inputs: [
+//       {
+//         internalType: "address",
+//         name: "_firstWinner",
+//         type: "address",
+//       },
+//       {
+//         internalType: "address",
+//         name: "_secondWinner",
+//         type: "address",
+//       },
+//       {
+//         internalType: "address",
+//         name: "_thirdWinner",
+//         type: "address",
+//       },
+//     ],
+//     name: "setWinners",
+//     outputs: [],
+//     stateMutability: "nonpayable",
+//     type: "function",
+//   },
+//   {
+//     inputs: [],
+//     name: "thirdWinner",
+//     outputs: [
+//       {
+//         internalType: "address",
+//         name: "",
+//         type: "address",
+//       },
+//     ],
+//     stateMutability: "view",
+//     type: "function",
+//   },
+//   {
+//     inputs: [],
+//     name: "totalPrizePool",
+//     outputs: [
+//       {
+//         internalType: "uint256",
+//         name: "",
+//         type: "uint256",
+//       },
+//     ],
+//     stateMutability: "view",
+//     type: "function",
+//   },
+//   {
+//     inputs: [],
+//     name: "tournamentStatus",
+//     outputs: [
+//       {
+//         internalType: "enum GamingTournament.TournamentStatus",
+//         name: "",
+//         type: "uint8",
+//       },
+//     ],
+//     stateMutability: "view",
+//     type: "function",
+//   },
+//   {
+//     stateMutability: "payable",
+//     type: "receive",
+//   },
+// ];
 
 interface Props {
   userAddress: string;
@@ -237,7 +237,7 @@ export const transferCUSD = async ({
 
         // Get Connected accounts, if not connected request connection.
     const provider = new BrowserProvider(
-      typeof window !== undefined ? window.ethereum : undefined
+      window.ethereum
     );
 
     // Get the signer from the provider
